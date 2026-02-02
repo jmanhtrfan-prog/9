b05591481c865e19fb532b1aa37f3fe83f27a3fa
jumana <jmanhtrfan@gmail.com>
2026-02-02 05:15:34 +0300

    Initial Commit

---
+++ 9T2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  9T2
+//
+//  Created by Jumana on 14/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 9T2/_T2App.swift
@@ -0,0 +1,17 @@
+//
+//  _T2App.swift
+//  9T2
+//
+//  Created by Jumana on 14/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct _T2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
